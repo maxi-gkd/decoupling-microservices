d81029f98b894d8cb0cacc901aa0873f84fd4706
maxi-gkd <maximiliano.kalaydjian@glovoapp.com>
2023-10-29 15:23:22 +0100

    Add springboot annotation

--- producer/src/main/java/org/example/ProducerApplication.java
+++ producer/src/main/java/org/example/ProducerApplication.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ProducerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
